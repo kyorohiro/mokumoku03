ad33b9c8f6fc8fb22431c8dd25d385b8ee18a7d7
kyorohiro <kyorohiro@gmail.com>
2020-09-27 04:00:45 +0900

    ..

--- myapp/lib/sample2.dart
+++ myapp/lib/sample2.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
+// https://stackoverflow.com/questions/51069712/how-to-know-if-a-widget-is-visible-within-a-viewport
 
 main( ) {
   runApp(
     MaterialApp(
       home:  Scaffold(
         appBar: AppBar(title: Text("Sample"),),
-        body: DynamiCGridView()
+        body: DynamiCGridView(MyDynamicGridViewClient())
       )
     )
   );
 }
 
-class DynamicGridViewClient {
-  int i=0;
+class MyDynamicGridViewClient extends  DynamicGridViewClient<String> {
+    int i=0;
   Future<List<String>> getData() async {
-    return List<String>.generate(100, (index) => "XX:${i++}");
+    return List<String>.generate(100, (index) => "ZZ:${i++}");
+  }
+  Widget createWidget(String v) {
+    return Container(child: Text(v),);
   }
 }
 
-DynamicGridViewClient client = new  DynamicGridViewClient();
+abstract class DynamicGridViewClient<X> {
+  Future<List<X>> getData();
+  Widget createWidget(X v);
+}
+
+//DynamicGridViewClient client = new  DynamicGridViewClient();
 
 class DynamiCGridView extends StatefulWidget {
+  DynamicGridViewClient client;
+  DynamiCGridView(this.client);
   @override
   _DynamiCGridViewState createState() => _DynamiCGridViewState();
 }
@@ -35,14 +46,14 @@ class _DynamiCGridViewState extends State<DynamiCGridView> {
       if(controller.position.pixels == controller.position.maxScrollExtent) {
         // add item?        
         print("=====================");
-        contents.addAll(await client.getData());
+        contents.addAll(await widget.client.getData());
         setState(() {
           
         });
       }
     });
     return GridView.count(
-      children: contents.map((e) => Container(child: Text(e),)).toList(),
+      children: contents.map((e) => widget.client.createWidget(e)).toList(),
       crossAxisCount: 3,
       controller: controller,
       );
