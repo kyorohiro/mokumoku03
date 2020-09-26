27bc9869ddc2c1b399d621e8e9377cd7123d054e
kyorohiro <kyorohiro@gmail.com>
2020-09-26 23:49:29 +0900

    ..

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -10,11 +10,20 @@ import 'dart:html' as html;
 
 /*
 [Memo]
-Firebase Storage Rule
+# Firebase Storage Rule
 rules_version = '2';
 service firebase.storage {
   match /b/{bucket}/o {
     match /{uid}/{allPaths=**} {
+      allow read, write: if request.auth != null;
+    }
+  }
+}
+# Firebase Store Rule
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /{document=**} {
       allow read, write: if request.auth != null;
     }
   }
@@ -90,7 +99,6 @@ class MyHome extends StatelessWidget {
       );
   }
 }
-
 
 
 
