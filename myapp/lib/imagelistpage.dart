e57b1aff74d05f219db2d7b6f5c1bde63c17090b
kyorohiro <kyorohiro@gmail.com>
2020-10-04 06:07:18 +0900

    extract image list page

---
+++ myapp/lib/imagelistpage.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/reigstpage.dart';
+import 'api_client.dart';
+import './loginpage.dart';
+import './logoutpage.dart';
+import './fileinput.dart' as fi;
+import './fileinput_web.dart' as fi;
+import './dynamicgridview.dart' as dyna;
+
+
+// String := image url
+class MyDynamicGridViewClient extends  dyna.DynamicGridViewClient<String> {
+  int i=0;
+  Object lastKey;
+  Future<List<String>> getData() async {
+    //
+    // TODO to reimplements about listFiles's lastkey  
+    var result = await listFiles(lastKey:lastKey);
+    lastKey = result.lastkey;
+    return  result.data;
+  }
+  Widget createWidget(String v) {
+    return  Container(
+            //color: Colors.black38,
+            decoration: BoxDecoration(
+              borderRadius:BorderRadius.circular(10.0) ,
+              border: Border.all(color:Colors.grey,width: 2)),
+            child: MyImageWidget(v)//Text('${e}'),
+          );
+    //return Container(child: MyImageWidget(v),);
+  }
+}
+
+class MyImageListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          child: dyna.DynamicGridView(MyDynamicGridViewClient(),3),
+      ),
+      appBar: AppBar(
+        title: Text("Home"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.logout), 
+            onPressed: (){
+              print("click logout");
+              Navigator.pushNamed(context, "/logout");
+            })
+        ],
+
+        
+        ),
+      //
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add_a_photo),
+        onPressed: () async {
+          // file upload   
+          var filedata = await fi.FileInputBuilderWeb().create().getFiles();
+          if(filedata != null && filedata.length > 0) {
+            var binary = await filedata.first.getBinaryData();
+            uploadBuffer(binary);
+          }
+        }),    
+      );
+  }
+}
+
+class MyImageWidget extends StatefulWidget {
+  String uuid;
+  MyImageWidget(this.uuid);
+
+  @override
+  _MyImageWidgetState createState() => _MyImageWidgetState();
+}
+
+class _MyImageWidgetState extends State<MyImageWidget> {
+  @override
+  Widget build(BuildContext context) {
+    getUrl(widget.uuid);
+    //return Container(child: Text("xx"),);
+    
+    return FutureBuilder(
+      future: getUrl(widget.uuid),
+      builder: (context, snapshot) {
+        if(snapshot.hasData) {
+          Uri d  = snapshot.data;
+          return Image.network(d.toString());
+        } else {
+          return  Container(child: Text("Loading.."),);
+        }
+      });
+    
+  }
+}
+
+
--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/reigstpage.dart';
+import 'package:myapp/reigstpage.dart' as page;
 import 'api_client.dart';
-import './loginpage.dart';
-import './logoutpage.dart';
+import './loginpage.dart' as page;
+import './logoutpage.dart' as page;
+import './imagelistpage.dart' as page;
+
 import './fileinput.dart' as fi;
 import './fileinput_web.dart' as fi;
 import './dynamicgridview.dart' as dyna;
-
 /*
 [Memo]
 # Firebase Storage Rule
@@ -33,6 +34,11 @@ service cloud.firestore {
 [ref]
 https://medium.com/@khreniak/cloud-firestore-security-rules-basics-fac6b6bea18e
 
+lighting_2_sutton_surrey.jpg
+https://free-images.com/display/lighting_2_sutton_surrey.html
+lighting_dept_high_st.jpg
+https://free-images.com/display/lighting_dept_high_st.html
+
 
 [Today TODO]
  - (asis ok) Logout 
@@ -47,100 +53,14 @@ void main() {
   runApp(
     MaterialApp(
       routes: <String, WidgetBuilder>{
-        "/login": (context) => LoginPage(),
-        "/regist":(context) => RegistPage(),
-        "/home": (context) => MyHome(),
-        "/logout": (context) => LogoutPage()
+        "/login": (context) => page.LoginPage(),
+        "/regist":(context) => page.RegistPage(),
+        "/home": (context) => page.MyImageListPage(),
+        "/logout": (context) => page.LogoutPage()
       },
       initialRoute: "/login",
     )
   );
-}
-
-// String := image url
-class MyDynamicGridViewClient extends  dyna.DynamicGridViewClient<String> {
-  int i=0;
-  Object lastKey;
-  Future<List<String>> getData() async {
-    //
-    // TODO to reimplements about listFiles's lastkey  
-    var result = await listFiles(lastKey:lastKey);
-    lastKey = result.lastkey;
-    return  result.data;
-  }
-  Widget createWidget(String v) {
-    return  Container(
-            //color: Colors.black38,
-            decoration: BoxDecoration(
-              borderRadius:BorderRadius.circular(10.0) ,
-              border: Border.all(color:Colors.grey,width: 2)),
-            child: MyImageWidget(v)//Text('${e}'),
-          );
-    //return Container(child: MyImageWidget(v),);
-  }
-}
-
-class MyHome extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          child: dyna.DynamicGridView(MyDynamicGridViewClient(),3),
-      ),
-      appBar: AppBar(
-        title: Text("Home"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.logout), 
-            onPressed: (){
-              print("click logout");
-              Navigator.pushNamed(context, "/logout");
-            })
-        ],
-
-        
-        ),
-      //
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo),
-        onPressed: () async {
-          // file upload   
-          var filedata = await fi.FileInputBuilderWeb().create().getFiles();
-          if(filedata != null && filedata.length > 0) {
-            var binary = await filedata.first.getBinaryData();
-            uploadBuffer(binary);
-          }
-        }),    
-      );
-  }
-}
-
-class MyImageWidget extends StatefulWidget {
-  String uuid;
-  MyImageWidget(this.uuid);
-
-  @override
-  _MyImageWidgetState createState() => _MyImageWidgetState();
-}
-
-class _MyImageWidgetState extends State<MyImageWidget> {
-  @override
-  Widget build(BuildContext context) {
-    getUrl(widget.uuid);
-    //return Container(child: Text("xx"),);
-    
-    return FutureBuilder(
-      future: getUrl(widget.uuid),
-      builder: (context, snapshot) {
-        if(snapshot.hasData) {
-          Uri d  = snapshot.data;
-          return Image.network(d.toString());
-        } else {
-          return  Container(child: Text("Loading.."),);
-        }
-      });
-    
-  }
 }
 
 
